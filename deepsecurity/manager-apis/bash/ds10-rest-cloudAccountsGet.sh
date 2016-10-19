7b066f18a2a28add50a85568bd41df3d79371727
424D57 <bryan_webster@trendmicro.com>
2016-10-19 11:20:34 -0400

    Add scripts to pull all cloud accounts

---
+++ deepsecurity/manager-apis/bash/ds10-rest-cloudAccountsGet.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# cloudAccountGet.sh dsmuser dsmpass manager address guiPort tenant
+username=$1
+password=$2
+tenant=$5
+
+# replace this with your DSM IP or FQDN
+DSMURL="$3:$4"
+
+
+echo "#####Login to DSM"
+if [[ -z $tenant ]]
+  then
+      SID=`curl -ks -H "Content-Type: application/json" -X POST "https://${DSMURL}/rest/authentication/login/primary" -d '{"dsCredentials":{"userName":"'${username}'","password":"'${password}'"}}'`
+  else
+      SID=`curl -ks -H "Content-Type: application/json" -X POST "https://${DSMURL}/rest/authentication/login" -d '{"dsCredentials":{"userName":"'${username}'","password":"'${password}'","tenantName":"'${tenant}'"}}'`
+fi
+
+
+
+curl -ks --cookie "sID=${SID}" -H "Content-Type: application/json" "Accept: application/json" -X Get "https://${DSMURL}/rest/cloudaccounts"
+
+curl -k -X DELETE https://$DSMURL/rest/authentication/logout?sID=$tempDSSID
+
+unset accesskey
+unset secretkey
+unset tempDSSID
+unset username
+unset password
+
+
