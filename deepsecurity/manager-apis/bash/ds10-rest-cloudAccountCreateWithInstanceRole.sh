565a6adaaf92a6829e8818b8c642f3cfd737cc61
424D57 <bryan_webster@trendmicro.com>
2016-12-19 14:24:43 -0600

    Add ds10 create cloud account

---
+++ deepsecurity/manager-apis/bash/ds10-rest-cloudAccountCreateWithInstanceRole.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# cloudAccountCreateWithInstanceRole.sh dsmuser dsmpass manager address guiPort tenant
+username=$1
+password=$2
+tenant=$5
+
+# replace this with your DSM IP or FQDN
+DSMURL="$3:$4"
+
+
+echo "#####Login to DSM"
+if [[ -z $tenant ]]
+  then
+      SID=`curl -ks -H "Content-Type: application/json" -X POST "https://${DSMURL}/rest/authentication/login/primary" -d '{"dsCredentials":{"userName":"'${username}'","password":"'${password}'"}}'`
+  else
+      SID=`curl -ks -H "Content-Type: application/json" -X POST "https://${DSMURL}/rest/authentication/login" -d '{"dsCredentials":{"userName":"'${username}'","password":"'${password}'","tenantName":"'${tenant}'"}}'`
+fi
+
+curl -ks --cookie "sID=${SID}" -H "Content-Type: application/json" "Accept: application/json" -X POST "https://${DSMURL}/rest/cloudaccounts/aws" -d '{"AddAwsAccountRequest":{"useInstanceRole":true}}'
+
+curl -k -X DELETE https://$DSMURL/rest/authentication/logout?sID=$tempDSSID
+
+unset SID
+unset username
+unset password
+
+
