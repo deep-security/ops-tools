cb193eee35047771345e82af84896fd920c4e112
bmw <bryan_webster@trendmicro.com>
2017-07-13 12:46:28 -0500

    Use hostDetailRetrieve instead of ByName

--- deepsecurity/manager-apis/powershell/get-hostIpsRules.ps1
+++ deepsecurity/manager-apis/powershell/get-hostIpsRules.ps1
@@ -26,14 +26,16 @@ catch {
 }
 
 $timestamp = Get-Date -Format yyyyMMddhhmmss
+$filename = "ipsrules$($timestamp).csv"
 
 $hts = $DSM.hostRetrieveAll($SID);
 
 foreach ($ht in $hts)
     {
-
-        $hostdetail = $DSM.hostDetailRetrieveByName($ht.name, [DSSOAP.EnumHostDetailLevel]::HIGH, $SID);
-
+        $hft = new-object DSSOAP.HostFilterTransport
+        $hft.type = [DSSOAP.EnumHostFilterType]::SPECIFIC_HOST
+        $hft.hostID = $ht.ID
+        $hostdetail = $DSM.hostDetailRetrieve($hft, [DSSOAP.EnumHostDetailLevel]::HIGH, $SID);
         if ($hostdetail.overallDpiStatus -like '*OFF*' -Or $hostdetail.overallDpiStatus -like 'Not Activated')
             {
                 continue
@@ -42,8 +44,6 @@ foreach ($ht in $hts)
         Write-Host "Checking details for hostID: $($ht.ID)"
         $hostPolicy = $DSM.securityProfileRetrieve($ht.securityProfileID, $SID)
         
-        $filename = "ipsrules$($timestamp).csv"
-
         foreach ($ipsrule in $hostPolicy.DPIRuleIDs)
             {
                 $csvline = New-Object PSObject;
